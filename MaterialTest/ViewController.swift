26dd7bd8d880af848f6c934ed578ca1c9c57c22c
Arif Hasnat <arifhasnat@arifs-mac.local>
2016-03-29 09:21:33 -0700

    Initial Commit

---
+++ MaterialTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MaterialTest
+//
+//  Created by Arif Hasnat on 3/29/16.
+//  Copyright © 2016 Arif Hasnat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
