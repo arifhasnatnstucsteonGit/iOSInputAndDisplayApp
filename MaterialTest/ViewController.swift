3d9ac677f31e89b771d4bca0178ea43599925536
arifhasnatnstucsteonGit <you@example.com>
2016-03-31 22:21:08 +0600

    complete input display app

--- MaterialTest/ViewController.swift
+++ MaterialTest/ViewController.swift
@@ -10,16 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+  
+
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var myLabel: UILabel!
+
+    
+    @IBAction func getName(sender: AnyObject) {
+        
+        myLabel.text="You'r name is  " + textField.text!
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
-}
+    
+   }
 
